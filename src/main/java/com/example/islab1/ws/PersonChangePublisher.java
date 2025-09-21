00006c3e3f0806b27aa5bad95ce9c7542e43a4dd
s1lentmol <artem.molchanov.1337@mail.ru>
2025-09-21 22:32:42 +0300

    set up WebSockets

---
+++ src/main/java/com/example/islab1/ws/PersonChangePublisher.java
@@ -0,0 +1,14 @@
+package com.example.islab1.ws;
+
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PersonChangePublisher {
+    private final SimpMessagingTemplate template;
+    public PersonChangePublisher(SimpMessagingTemplate template) { this.template = template; }
+    public void broadcastChange() {
+        // Клиенты подпишутся на /topic/persons и просто перезагрузят таблицу
+        template.convertAndSend("/topic/persons", "changed");
+    }
+}
