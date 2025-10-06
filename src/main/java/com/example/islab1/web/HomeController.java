3281cc37ed6b16c4b8b277c57408dfcca4c9a03a
s1lentmol <artem.molchanov.1337@mail.ru>
2025-10-06 10:41:45 +0300

    Add favicon endpoint to HomeController

--- src/main/java/com/example/islab1/web/HomeController.java
+++ src/main/java/com/example/islab1/web/HomeController.java
@@ -1,5 +1,7 @@
 package com.example.islab1.web;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -8,5 +10,10 @@ public class HomeController {
     @GetMapping("/")
     public String home() {
         return "redirect:/persons";
+    }
+
+    @GetMapping("/favicon.ico")
+    public ResponseEntity<Void> favicon() {
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 }
